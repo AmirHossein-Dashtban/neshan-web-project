5551c4c6e4c959656ad30fe209a19228c434e350
AmirHossein_Dashtban <amirhossein.namaghi@gmail.com>
2025-01-19 20:17:22 +0330

    [feat] Add loading spinner to input

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -4,17 +4,23 @@ import { useContext, useEffect, useRef, useState } from "react";
 import { MapContext } from "@/context/MapContext";
 import { fetchSearchResult } from "@/api/fetchSearchResult";
 import { Input } from "../ui/Input";
-import CancelIcon from "../../../public/CancelIcon";
 import { removeMarker } from "@/utils/function/markerUtils";
 import primaryMarkerImage from "../../app/Restaurant.png";
+import foodImage from "@/../public/food-two.jpg";
 import { fitMapToBounds } from "@/utils/function/fitBounds";
 import { MarkerWithPopup } from "../MarkerWithPopup/MarkerWithPopup";
+import MainRouting from "../MianRouting/MainRouting";
+import { clearMap } from "@/utils/function/setRoute";
+import { X } from "lucide-react";
+import Spinner from "../LoadingSpinner/Spinner";
 
 export default function SearchInput() {
 	const [searchValue, setSearchValue] = useState("");
 	const markerListRef = useRef([]);
 	const [data, setData] = useState();
 	const { map } = useContext(MapContext);
+	const [isRouting, setIsRouting] = useState(null);
+	const [isLoading, setIsLoading] = useState("idle");
 
 	useEffect(() => {
 		const params = new URLSearchParams(window.location.search);
@@ -37,21 +43,21 @@ export default function SearchInput() {
 			window.history.pushState(null, "", `?${params.toString()}`);
 		} else {
 			const params = new URLSearchParams(window.location.search);
-			params.delete("search");
-			window.history.pushState(null, "", `${params.toString()}`);
+			params.delete("?search");
+			window.history.pushState(null, "", window.location.pathname);
 		}
 		if (searchValue) {
+			setIsLoading("loading");
 			fetchSearchResult(
 				`رستوران ${searchValue}`,
 				36.32708394227474,
 				59.5528219217324
 			).then((res) => {
+				setIsLoading("idle");
 				if (!ignore) {
 					const items = res.items;
 					items.forEach((item) => points.push(item.location));
 					fitMapToBounds(map, points);
-					console.log(res);
-
 					setData(res.items);
 				}
 			});
@@ -62,12 +68,15 @@ export default function SearchInput() {
 
 	const handleInputChange = (e) => {
 		setSearchValue(e.target.value);
+		setIsRouting(null);
+		clearMap(map);
 	};
 
 	const handleCancelClick = () => {
 		setSearchValue("");
 		setData(null);
-		removeMarker(markerListRef);
+		setIsRouting(null);
+		clearMap(map);
 	};
 
 	return (
@@ -79,25 +88,40 @@ export default function SearchInput() {
 				value={searchValue}
 				onChange={handleInputChange}
 			/>
-			<CancelIcon
-				className="bg-red cursor-pointer mx-1 rounded hover:bg-slate-300 p-1"
-				onClick={handleCancelClick}
-			/>
+
+			{isLoading === "loading" ? (
+				<Spinner />
+			) : (
+				<X
+					className="bg-red text-white cursor-pointer mx-1 rounded hover:bg-slate-700 p-1"
+					onClick={handleCancelClick}
+				/>
+			)}
 
 			{data?.map((item, index) => {
 				return (
 					<MarkerWithPopup
 						key={index}
 						map={map}
-						imageUrl={primaryMarkerImage.src}
+						markerImageUrl={primaryMarkerImage.src}
 						initialLngLat={{
 							lng: item.location.x,
 							lat: item.location.y,
 						}}
 						item={item}
+						popupImage={foodImage.src}
+						setLngLat={setIsRouting}
+						setData={setData}
 					/>
 				);
 			})}
+
+			{isRouting !== null && (
+				<MainRouting
+					lngLat={isRouting}
+					handleCancelShowToast={handleCancelShowToast}
+				/>
+			)}
 		</>
 	);
 }
